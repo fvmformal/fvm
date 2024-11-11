2edc13fff8277083cc9bef8dbac78acb287a3126
Hipolito Guzman <hguzman@us.es>
2024-11-12 00:29:50 +0100

    Update src/builder/helpers.py from previous commit
    This file should have been in the previous commit :)

--- src/builder/helpers.py
+++ src/builder/helpers.py
@@ -1,6 +1,14 @@
 # Helper functions
 import inspect
+from humanize.time import precisedelta
 
 def getscriptname():
     scriptname = inspect.stack()[-1][1]
     return(scriptname)
+
+def readable_time(seconds):
+    if seconds <= 1:
+        ret = "{:.2f}".format(seconds)
+    else:
+        ret = precisedelta(seconds)
+    return ret
