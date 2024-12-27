fa6990a1cf71c5fd01fba1e8b7927aa427a196f3
marcos <marlopgar40@alum.us.es>
2024-12-27 01:39:12 +0100

    Add parameterized properties to interpolator

--- examples/linearinterpolator/formal.py
+++ examples/linearinterpolator/formal.py
@@ -4,8 +4,4 @@ fvm = fvmframework()
 fvm.add_vhdl_sources("examples/linearinterpolator/*.vhd")
 fvm.add_psl_sources("examples/linearinterpolator/*.psl")
 fvm.set_toplevel("interpolator")
-# TODO : evaluate in detail if what RDC/CDC is seeing is a problematic issue or
-# not, if it can be denoted to warning, etc
-fvm.skip("resets")
-fvm.skip("clocks")
 fvm.run()
