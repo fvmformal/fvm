2705132adae3739b9f8a13b734a2e5ba95e634d8
marcos <marlopgar40@alum.us.es>
2024-12-28 21:32:56 +0100

    Add parameterized properties to synchronous FIFO

--- examples/fifo_sync/formal.py
+++ examples/fifo_sync/formal.py
@@ -9,16 +9,16 @@ subprocess.run(['git', 'clone', '--recurse-submodules=:src',
 
 fvm.add_vhdl_sources("open-logic/src/base/vhdl/*.vhd")
 
-fvm.add_psl_source("examples/fifo_sync/olo_base_fifo_sync.psl")
+fvm.add_psl_sources("examples/fifo_sync/*.psl")
 
 fvm.set_toplevel("olo_base_fifo_sync")
 fvm.add_config("olo_base_fifo_sync", "config_width_3_depth_2", {"Width_g": 3, "Depth_g": 8})
 
-#fvm.skip('lint')
-#fvm.skip('friendliness')
+fvm.skip('lint')
+fvm.skip('friendliness')
 fvm.skip('reachability')
-#fvm.skip('resets')
-#fvm.skip('clocks')
+fvm.skip('resets')
+fvm.skip('clocks')
 fvm.disable_coverage('signoff')
 fvm.disable_coverage('reachability')
 fvm.run()
