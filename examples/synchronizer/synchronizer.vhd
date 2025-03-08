26aff05f9458df0475e416bb70793dafc69c002f
Hipolito Guzman <hguzman@us.es>
2025-03-08 01:30:22 +0100

    sychronizer: make STAGES positive, not integer
    Also commented out the assert about the value of STAGES, because we
    want to see if we can find any issues when STAGES=1

--- examples/synchronizer/synchronizer.vhd
+++ examples/synchronizer/synchronizer.vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_1164.all;
 -- Synchronizer to pass single-bit signals between two clock domains
 entity synchronizer is
     generic (
-        STAGES    : integer    := 2;   -- Number of flip-flops (typically 2 or 3)
+        STAGES    : positive   := 2;   -- Number of flip-flops (typically 2 or 3)
         RST_VALUE : std_ulogic := '0'  -- Reset value for the flip-flops
     );
     port (
@@ -21,12 +21,15 @@ architecture synchronizer_arch of synchronizer is
 
 begin
 
+    -- Commented out the following assertion because we want to see if we can
+    -- find issues when STAGES=1
+    --
     -- A synchronizer needs at least two flip-flops
-    assert STAGES >= 2
+    --assert STAGES >= 2
         --report synchronizer'PATH_NAME & " bad value for STAGES (" &  -- not supported by propcheck :(
-        report "synchronizer:" & " bad value for STAGES (" &
-               integer'IMAGE(STAGES) & "), should be at least 2"
-        severity failure;
+    --    report "synchronizer:" & " bad value for STAGES (" &
+    --           integer'IMAGE(STAGES) & "), should be at least 2"
+    --    severity failure;
 
     -- Input enters the LSB of reg, then gets shifted towards output once per clk cycle
     n_reg <= reg(reg'HIGH-1 downto reg'LOW) & input;
