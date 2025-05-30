972b36db243c696d16212ea7feb6197773db4edb
Hipolito Guzman <hguzman@us.es>
2025-05-30 13:41:46 +0200

    Fix concepts/assertion_decomposition
    Since the interpolator has a latch, let's just reuse the interpolator
    we have fixed in examples/linearinterpolator

--- concepts/assertion_decomposition/formal.py
+++ concepts/assertion_decomposition/formal.py
@@ -1,7 +1,8 @@
 from fvm import fvmframework
 
 fvm = fvmframework()
-fvm.add_vhdl_sources("concepts/assertion_decomposition/*.vhd")
+fvm.add_vhdl_sources("examples/linearinterpolator/*.vhd")
+fvm.add_psl_sources("examples/linearinterpolator/*.psl")
 fvm.add_psl_sources("concepts/assertion_decomposition/*.psl")
 fvm.set_toplevel("interpolator")
 fvm.run()
