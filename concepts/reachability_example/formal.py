25140cdda48c5960e1d30784aa653853bbf639ac
marcos <marlopgar40@alum.us.es>
2025-06-02 17:14:40 +0200

    concepts: Adapt reachability_example to sby #272

--- concepts/reachability_example/formal.py
+++ concepts/reachability_example/formal.py
@@ -2,7 +2,7 @@ from fvm import fvmframework
 
 fvm = fvmframework()
 fvm.add_vhdl_source("concepts/reachability_example/counter.vhd")
-fvm.add_psl_source("concepts/reachability_example/counter.psl")
+fvm.add_psl_sources("concepts/reachability_example/*.psl")
 fvm.set_toplevel("counter")
 # Reachability is skipped because is going to fail,
 # and we don't want to break the CI.
