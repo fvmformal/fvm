96e2f62ed6de0f5356a8327f9ff8d6cf01beda02
marcos <marlopgar40@alum.us.es>
2025-07-24 11:54:39 +0200

    Fix interpolator

--- concepts/assertion_decomposition/formal.py
+++ concepts/assertion_decomposition/formal.py
@@ -2,7 +2,6 @@ from fvm import fvmframework
 
 fvm = fvmframework()
 fvm.add_vhdl_sources("examples/linearinterpolator/*.vhd")
-fvm.add_psl_sources("examples/linearinterpolator/*.psl")
 fvm.add_psl_sources("concepts/assertion_decomposition/*.psl")
 fvm.set_toplevel("interpolator")
 fvm.run()
