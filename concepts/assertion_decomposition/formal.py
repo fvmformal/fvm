e0861499bec0ad9a8ea40a103253c3970304039f
marcos <marlopgar40@alum.us.es>
2025-07-24 12:35:33 +0200

    Add clock and reset domain to linearinterpolator

--- concepts/assertion_decomposition/formal.py
+++ concepts/assertion_decomposition/formal.py
@@ -4,4 +4,11 @@ fvm = fvmframework()
 fvm.add_vhdl_sources("examples/linearinterpolator/*.vhd")
 fvm.add_psl_sources("concepts/assertion_decomposition/*.psl")
 fvm.set_toplevel("interpolator")
+
+fvm.add_clock("clk", period = 10)
+fvm.add_clock_domain("clk", ["inferior", "superior", "valid"])
+
+fvm.add_reset("rst", asynchronous=True, active_high=True)
+fvm.add_reset_domain("rst", ["inferior", "superior", "valid"])
+
 fvm.run()
