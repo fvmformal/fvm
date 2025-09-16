437ace07909725db28d585261c01d33fbd5566cd
marcos <marlopgar40@alum.us.es>
2025-09-16 17:57:09 +0200

    Delete pylint warning and add docstring to parse_resets.py #115

--- fvm/parsers/parse_resets.py
+++ fvm/parsers/parse_resets.py
@@ -1,15 +1,28 @@
+"""
+Parser for resets step results.
+
+This module extracts checks and organizes them by
+categories such as Violation, Caution, Evaluation, etc.
+
+It is specifically for Questa RDC results.
+"""
 import re
 
 def parse_resets_results(file_path):
+    """
+    Parse a resets step results file and return a dictionary with check details.
 
+    :param file_path: Path to the resets results file.
+    :type file_path: str
+    :return: Dictionary with categories and their check details.
+    :rtype: dict
+    """
     with open(file_path, 'r', encoding='utf-8') as file:
         content = file.read()
 
-    rdc_summary_pattern = re.compile(
-        r'Total number of checks:\s*\((\d+)\)'
-    )
     category_pattern = re.compile(
-        r'(Violation|Caution|Evaluation|Resolved - Waived or Verified Status|Filtered)\s*\((\d+)\)\n'
+        r'(Violation|Caution|Evaluation|Resolved - Waived or Verified Status|Filtered)\s*'
+        r'\((\d+)\)\n'
         r'-----------------------------------------------------------------\n'
         r'([\s\S]*?)\n\n',
         re.DOTALL
@@ -28,7 +41,9 @@ def parse_resets_results(file_path):
     for category in categories:
         category_name = category[0]
         category_count = int(category[1])
-        category_details = category[2].strip().split('\n') if category[2].strip() != "<None>" else []
+
+        raw_details = category[2].strip()
+        category_details = raw_details.split('\n') if raw_details != "<None>" else []
 
         resets_results[category_name]["count"] = category_count
 
