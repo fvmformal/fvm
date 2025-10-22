6704b3392d5dea211b4627f520334ed2c07376cf
Hipolito Guzman <hguzman@us.es>
2025-10-22 12:33:02 +0200

    doc: change order of commandline args and some descriptions

--- src/fvm/argument_parser.py
+++ src/fvm/argument_parser.py
@@ -11,27 +11,27 @@ def create_parser():
     """
     # Configure the argument parser
     parser = argparse.ArgumentParser(description='Run the formal tools')
-    parser.add_argument('-v', '--verbose', default=False, action='store_true',
-            help='Show full tool outputs. (default: %(default)s)')
-    parser.add_argument('-l', '--list', default=False, action='store_true',
-            help='Only list available methodology steps, but do not execute them. (default: %(default)s)')
-    parser.add_argument('-o', '--outdir', default = "fvm_out",
-            help='Output directory. (default: %(default)s)')
     parser.add_argument('-d', '--design',
             help='If set, run the specified design. If unset, run all designs. (default: %(default)s)')
     parser.add_argument('-s', '--step',
             help='If set, run the specified step. If unset, run all steps. (default: %(default)s)')
     parser.add_argument('-c', '--cont', default=False, action='store_true',
             help='Continue with next steps even if errors are detected. (default: %(default)s)')
+    parser.add_argument('-v', '--verbose', default=False, action='store_true',
+            help='Show full tool outputs. (default: %(default)s)')
+    parser.add_argument('-l', '--list', default=False, action='store_true',
+            help='Only list available methodology steps, but do not execute them. (default: %(default)s)')
+    parser.add_argument('-o', '--outdir', default = "fvm_out",
+            help='Output directory. (default: %(default)s)')
     parser.add_argument('-g', '--gui', default=False, action='store_true',
             help='Show tool results with GUI after tool execution. (default: %(default)s)')
     parser.add_argument('-n', '--guinorun', default=False, action='store_true',
             help='Show already existing tool results with GUI, without running the tools again. (default: %(default)s)')
     parser.add_argument('--show', default=False, action='store_true',
-            help='Show the HTML dashboard after execution. (default: %(default)s)')
+            help='Show the HTML dashboard after execution of the formal tools. (default: %(default)s)')
     parser.add_argument('--shownorun', default=False, action='store_true',
-            help='Show the existing HTML dashboard, without running. (default: %(default)s)')
+            help='Show the existing HTML dashboard, without running the formal tools. (default: %(default)s)')
     parser.add_argument('--globalshow', default=False, action='store_true',
-            help='Show the existing HTML dashboard of every design, without running. (default: %(default)s)')
+            help='Show the existing HTML dashboard of every design in the output directory, without running the formal tools. (default: %(default)s)')
 
     return parser
