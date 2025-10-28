ea1ba246c3dc95ff432ce2e4a26aa062a6716ae6
marcos <marlopgar40@alum.us.es>
2025-10-28 15:03:40 +0100

    Only one process in verilog counters

--- examples/counter_sv/counter.sv
+++ examples/counter_sv/counter.sv
@@ -1,29 +1,17 @@
 module counter #(
     parameter int MAX_COUNT = 128
 ) (
-    input  logic        clk,
-    input  logic        rst,
+    input  logic        clk, rst,
     output logic [7:0]  Q
 );
 
-    logic [7:0] count;
-    logic [7:0] p_count;
-
-    always_ff @(posedge clk or posedge rst) begin
+    always_ff @(posedge clk or posedge rst)
         if (rst)
-            count <= 8'd0;
+            Q <= 0;
+        else if (Q == MAX_COUNT)
+            Q <= 0;
         else
-            count <= p_count;
-    end
-
-    always_comb begin
-        if (count == MAX_COUNT)
-            p_count = 8'd0;
-        else
-            p_count = count + 1;
-    end
-
-    assign Q = count;
+            Q <= Q + 1;
 
     // ========================================================
     // SVA Properties
--- examples/counter_v_psl/counter.v
+++ examples/counter_v_psl/counter.v
@@ -1,30 +1,18 @@
 module counter #(
     parameter MAX_COUNT = 128
 ) (
-    input  wire        clk,
-    input  wire        rst,
-    output reg  [7:0]  Q
+    input  wire clk,
+    input  wire rst,
+    output reg [7:0] Q
 );
 
-    reg [7:0] count;
-    reg [7:0] p_count;
-
-    always @(posedge clk or posedge rst) begin
-        if (rst)
-            count <= 8'd0;
-        else
-            count <= p_count;
-    end
-
-    always @(*) begin
-        if (count == MAX_COUNT)
-            p_count = 8'd0;
-        else
-            p_count = count + 1;
-    end
-
-    always @(*) begin
-        Q = count;
-    end
+always @(posedge clk or posedge rst) begin
+    if (rst)
+        Q <= 8'd0;
+    else if (Q == MAX_COUNT)
+        Q <= 8'd0;
+    else
+        Q <= Q + 1;
+end
 
 endmodule
