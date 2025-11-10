34210e1ccc6e591c2de40118e61051b622026d17
Hipolito Guzman <hguzman@us.es>
2025-11-10 16:36:54 +0100

    Remove training folder as it is no longer needed

--- training/00-counter/formal.py
+++
@@ -1,8 +0,0 @@
-from src.builder.framework import FvmFramework
-
-fvm = FvmFramework()
-fvm.add_vhdl_source("examples/00-counter/counter.vhd")
-fvm.set_toplevel("counter")
-fvm.run()
-#fvm.setup()
-#fvm.run_step("reachability")
--- training/05-uart_tx/formal.py
+++
@@ -1,7 +0,0 @@
-from src.builder.framework import FvmFramework
-
-fvm = FvmFramework()
-fvm.add_vhdl_sources("training/05-uart_tx/*.vhd")
-fvm.add_psl_sources("training/05-uart_tx/*.psl")
-fvm.set_toplevel("uart_tx")
-fvm.run()
