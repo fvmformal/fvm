36b5f327406875dba925e6f0179371cf4d07fe37
Hipolito Guzman <hguzman@us.es>
2025-11-10 17:45:54 +0100

    Use new path for drom sequences in concepts/adding_drom_sources

--- concepts/adding_drom_sources/formal.py
+++ concepts/adding_drom_sources/formal.py
@@ -3,7 +3,7 @@ from fvmframework import FvmFramework
 fvm = FvmFramework()
 fvm.add_vhdl_sources("concepts/wishbone_sequence/*.vhd")
 fvm.add_psl_source("concepts/wishbone_sequence/properties.psl")
-fvm.add_drom_source("drom/wishbone_classic_read.json")
+fvm.add_drom_source("drom_sequences/wishbone_classic_read.json")
 fvm.set_toplevel("wishbone")
 fvm.allow_failure("rulecheck")
 fvm.set_coverage_goal("reachability", 75)
