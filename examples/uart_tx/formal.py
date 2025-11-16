3036ad22a5ccbab160384527f82e8662cd031bc6
marcos <marlopgar40@alum.us.es>
2025-11-16 13:16:25 +0100

    examples: Use drom2psl in the uart

--- examples/uart_tx/formal.py
+++ examples/uart_tx/formal.py
@@ -3,6 +3,7 @@ from fvm import FvmFramework
 fvm = FvmFramework()
 fvm.add_vhdl_sources("examples/uart_tx/*.vhd")
 fvm.add_psl_sources("examples/uart_tx/*.psl", flavor="vhdl")
+fvm.add_drom_sources("examples/uart_tx/*.json", flavor="vhdl")
 fvm.set_toplevel("uart_tx")
 # Rulecheck fails due to symbolic constants are interpreted as
 # declaration undriven.
