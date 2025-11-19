1bfbd67f20a12df91a05f1acccd2ac67eceb4a79
marcos <marlopgar40@alum.us.es>
2025-11-19 14:50:53 +0100

    examples: Fix change counter path

--- examples/counter/formal.py
+++ examples/counter/formal.py
@@ -1,7 +1,7 @@
 from fvm import FvmFramework
 
 fvm = FvmFramework()
-fvm.add_vhdl_source("examples/countervunit/counter.vhd")
-fvm.add_psl_source("examples/countervunit/counter_properties.psl", flavor="vhdl")
+fvm.add_vhdl_source("examples/counter/counter.vhd")
+fvm.add_psl_source("examples/counter/counter_properties.psl", flavor="vhdl")
 fvm.set_toplevel("counter")
 fvm.run()
