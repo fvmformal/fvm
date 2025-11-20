f05890dfac7bc4b4ccb21a89ac36b09028beba5d
marcos <marlopgar40@alum.us.es>
2025-11-20 03:24:38 +0100

    sdram: TLM #235

--- examples/sdram/formal.py
+++ examples/sdram/formal.py
@@ -8,8 +8,8 @@ subprocess.run(['git', 'clone',
 'https://github.com/nullobject/sdram-fpga'])
 
 fvm.add_vhdl_source("sdram-fpga/sdram.vhd")
-
-fvm.add_psl_source("examples/sdram/sdram.psl", flavor="vhdl")
+fvm.add_psl_sources("examples/sdram/*.psl", flavor="vhdl")
+fvm.add_drom_sources("examples/sdram/*.json", flavor="vhdl")
 
 fvm.set_toplevel("sdram")
 fvm.add_config("sdram", "config_freq_10_desl_200", {"CLK_FREQ": 10.0, "T_DESL": 200.0})
