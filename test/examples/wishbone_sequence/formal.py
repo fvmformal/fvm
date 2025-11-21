30709ba83507893753498278e28b0f901ed174f3
Hipolito Guzman <hguzman@us.es>
2025-11-21 22:36:19 +0100

    Move concepts/wishbone_sequence to test/

--- test/examples/wishbone_sequence/formal.py
+++ test/examples/wishbone_sequence/formal.py
@@ -1,8 +1,8 @@
 from fvm import FvmFramework
 
 fvm = FvmFramework()
-fvm.add_vhdl_sources("concepts/wishbone_sequence/*.vhd")
-fvm.add_psl_sources("concepts/wishbone_sequence/*.psl", flavor="vhdl")
+fvm.add_vhdl_sources("test/examples/wishbone_sequence/*.vhd")
+fvm.add_psl_sources("test/examples/wishbone_sequence/*.psl", flavor="vhdl")
 fvm.set_toplevel("wishbone")
 fvm.skip("rulecheck")
 fvm.set_coverage_goal("reachability", 70)
