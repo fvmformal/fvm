2e05d760d7f4975a5c89d45ab35e0c2c0fa5216e
Hipolito Guzman <hguzman@us.es>
2025-11-21 22:39:25 +0100

    Change text/examples so they can be run from the root folder

--- test/examples/counter_psl_in_vhdl/formal.py
+++ test/examples/counter_psl_in_vhdl/formal.py
@@ -1,6 +1,6 @@
 from fvm import FvmFramework
 
 fvm = FvmFramework()
-fvm.add_vhdl_source("counter.vhd")
+fvm.add_vhdl_source("test/examples/counter_psl_in_vhdl/counter.vhd")
 fvm.set_toplevel("counter")
 fvm.run()
--- test/examples/inheriting_vunits_fail/formal.py
+++ test/examples/inheriting_vunits_fail/formal.py
@@ -2,7 +2,7 @@ from fvm import FvmFramework
 
 fvm = FvmFramework()
 fvm.add_vhdl_sources("examples/uart_tx/*.vhd")
-fvm.add_psl_sources("concepts/inheriting_vunits/*.psl", flavor="vhdl")
+fvm.add_psl_sources("test/examples/inheriting_vunits_fail/*.psl", flavor="vhdl")
 fvm.set_toplevel("uart_tx")
 
 # There is a failure in prove.formalcover
